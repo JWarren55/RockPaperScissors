30a79d81982adca082edf8dd9eddda6f69ba1257
JWarren55 <jonwwarren55@gmail.com>
2025-11-11 09:51:55 -0600

    Add getComputerChoice()
    Function makes a random number and prints Rock, Paper or Scissors

---
+++ rockPaperScissors.js
@@ -0,0 +1,10 @@
+function getComputerChoice() {
+    number = Math.random();
+    if (.6 < number) {
+        return "Rock"
+    } else if (.3 < number) {
+        return "Paper"
+    } else {
+        return "Scissors"
+    }
+}
